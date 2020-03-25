3a78e6c6cc27a7631b96624157e8d6eb3eb058f0
nanji <1850094299@qq.com>
2020-03-25 10:03:55 +0800

    topic

---
+++ src/main/java/com/atguigu/activemq/queue/JmsConsumer_Topic_persist.java
@@ -0,0 +1,30 @@
+package com.atguigu.activemq.queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import java.io.IOException;
+
+public class JmsConsumer_Topic_persist {
+    public static final String ACTIVEMQ_URL = "tcp://localhost:61616";
+    public static final String TOPIC_NAME = "topic-persist";
+
+    public static void main(String[] args) throws JMSException, IOException {
+        System.out.println("**********z3");
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.setClientID("z3");
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Topic topic = session.createTopic(TOPIC_NAME);
+        TopicSubscriber topicSubscriber = session.createDurableSubscriber(topic, "remakr....");
+        connection.start();
+        Message message = topicSubscriber.receive();
+        while (null != message) {
+            TextMessage textMessage = (TextMessage) message;
+            System.out.println("收到的持久化topic:" + textMessage.getText());
+            message = topicSubscriber.receive(1000L);
+        }
+        session.close();
+        connection.close();
+    }
+}
--- src/main/java/com/atguigu/activemq/queue/JmsProduce_topic_persist.java
+++ src/main/java/com/atguigu/activemq/queue/JmsProduce_topic_persist.java
@@ -3,43 +3,39 @@ package com.atguigu.activemq.queue;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
+import java.util.HashSet;
 
-public class JmsProduce {
+public class JmsProduce_topic_persist {
     public static final String ACTIVEMQ_URL = "tcp://localhost:61616";
-    public static final String QUEUE_NAME = "queue01";
+    public static final String TOPIC_NAME = "topic-persist";
 
     public static void main(String[] args) throws JMSException {
         //1.创建链接工厂,按照给定的url地址，采用默认的用户名和秘密
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
         //2.通过连接工厂，获得连接的connection并启动访问
         Connection connection = activeMQConnectionFactory.createConnection();
-        connection.start();
         //3.创建会话session
         //两个参数，第一个叫事务，第二个叫签收
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         //4.创建目的地（具体是队列还是主题 topic)
         //Collection collection=new ArrayList();
-        Queue queue = session.createQueue(QUEUE_NAME);
+//        Queue queue = session.createQueue(TOPIC_NAME);
+        Topic topic = session.createTopic(TOPIC_NAME);
         //5. 创建消息的生产者
-        MessageProducer messageProducer = session.createProducer(queue);
-        //非持久化
-//        messageProducer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+        MessageProducer messageProducer = session.createProducer(topic);
         messageProducer.setDeliveryMode(DeliveryMode.PERSISTENT);
+        connection.start();
         //6. 通过使用messageProducer成产3条消息发送到MQ的队列里面
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 3; i++) {
             //7. 创建消息，好比学生按照阳哥的要修写好的面试题消息 。
-            TextMessage textMessage = session.createTextMessage("text---Msg ---" + i);//理解为一个字符串
-            textMessage.setStringProperty("c01", "vip");
+            TextMessage textMessage = session.createTextMessage("Topic _ name---" + i);//理解为一个字符串
             //8. 通过messageProducer发送给mq
             messageProducer.send(textMessage);
-            MapMessage mapMessage = session.createMapMessage();
-            mapMessage.setString("k1", "mapMessage v1");
-            messageProducer.send(mapMessage);
         }
         messageProducer.close();
         session.close();
         connection.close();
-        System.out.println("********消息发送到mq完成 ");
-
+        System.out.println("********TOPIC_NAME 消息发送到MQ完成");
+        HashSet<String> a = new HashSet<>();
     }
 }
